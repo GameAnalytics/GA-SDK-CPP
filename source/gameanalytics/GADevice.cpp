94592be157cdd77e5f0ee4b351d9b3d54734b70c
Martin Treacy-Schwartz <thetreacyschwartz@gmail.com>
2016-10-12 11:35:03 +0200

    version updated

--- source/gameanalytics/GADevice.cpp
+++ source/gameanalytics/GADevice.cpp
@@ -18,7 +18,7 @@ namespace gameanalytics
         std::string GADevice::_sdkGameEngineVersion;
         std::string GADevice::_gameEngineVersion;
         std::string GADevice::_connectionType = "offline";
-        const std::string GADevice::_sdkWrapperVersion = "cpp 1.0.1";
+        const std::string GADevice::_sdkWrapperVersion = "cpp 1.1.0";
 
         void GADevice::setSdkGameEngineVersion(const std::string& sdkGameEngineVersion)
         {
