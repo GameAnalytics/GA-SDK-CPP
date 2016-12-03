028c20b09effcefe2a47b35de97b89a28854280b
Martin Treacy-Schwartz <mts@gameanalytics.com>
2016-12-03 23:54:09 +0100

    removed unused files

--- source/dependencies/cppnetlib/0.11.2/source/build_vc120_x86.bat
+++
@@ -1,5 +0,0 @@
-rmdir build
-mkdir build
-cd build
-cmake -G "Visual Studio 12" -DBOOST_ROOT="C:\boost_1_60_0" -DOPENSSL_LIBRARIES=..\..\..\..\openssl\1.0.2h\libs\win32\vc120_x86_release -DOPENSSL_INCLUDE_DIR=..\..\..\..\openssl\1.0.2h\include ..
-cd..
--- source/dependencies/cppnetlib/0.11.2/source/build_vc140_x86.bat
+++
@@ -1,5 +0,0 @@
-rmdir build
-mkdir build
-cd build
-cmake -G "Visual Studio 14" -DBOOST_ROOT="C:\boost_1_60_0" -DOPENSSL_LIBRARIES=..\..\..\..\openssl\1.0.2h\libs\win32\vc140_x86_release -DOPENSSL_INCLUDE_DIR=..\..\..\..\openssl\1.0.2h\include ..
-cd..
