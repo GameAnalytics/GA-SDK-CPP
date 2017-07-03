afcbb9243c93c6bb435774740bff8464ffc6d683
Martin Treacy-Schwartz <thetreacyschwartz@gmail.com>
2017-07-03 14:00:16 +0200

    added defold for switching to http on mac

--- source/gameanalytics/GameAnalytics.cpp
+++ source/gameanalytics/GameAnalytics.cpp
@@ -235,8 +235,9 @@ namespace gameanalytics
                 logging::GALogger::w("Could not ensure/validate local event database: " + device::GADevice::getWritablePath());
             }
 
-            // TODO: Get HTTPS working on air for mac
-            if(device::GADevice::getRelevantSdkVersion().find("air ") != std::string::npos &&
+            // TODO: Get HTTPS working on air and defold for mac
+            if((device::GADevice::getRelevantSdkVersion().find("air ") != std::string::npos ||
+                device::GADevice::getRelevantSdkVersion().find("defold ") != std::string::npos) &&
                 device::GADevice::getBuildPlatform().find("mac_osx") != std::string::npos)
             {
                 http::GAHTTPApi::switchProtocolToHttp();
