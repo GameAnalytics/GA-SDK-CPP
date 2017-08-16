e1c359c743f62f6322d011fcc86e09b5de645a5b
Martin Treacy-Schwartz <thetreacyschwartz@gmail.com>
2017-08-16 11:22:59 +0200

    fixed https for libcurl for mac

--- source/dependencies/openssl/1.0.2h/build.sh
+++ source/dependencies/openssl/1.0.2h/build.sh
@@ -9,20 +9,20 @@ tar -xvzf openssl-$OPENSSL_VERSION.tar.gz
 mv openssl-$OPENSSL_VERSION openssl_x86_64
 cd openssl_i386
 # mac
-#./Configure darwin-i386-cc -static
+./Configure darwin-i386-cc -static
 
 # linux
-./Configure -m32 linux-generic32 -static
+#./Configure -m32 linux-generic32 -static
 make
 cd ../
 cd openssl_x86_64
 # mac
-#./Configure darwin64-x86_64-cc -static
+./Configure darwin64-x86_64-cc -static
 
 # linux
-./Configure -m64 linux-generic64 -static
+#./Configure -m64 linux-generic64 -static
 make
 cd ../
-#lipo -create openssl_i386/libcrypto.a openssl_x86_64/libcrypto.a -output libcrypto.a
-#lipo -create openssl_i386/libssl.a openssl_x86_64/libssl.a -output libssl.a
+lipo -create openssl_i386/libcrypto.a openssl_x86_64/libcrypto.a -output libcrypto.a
+lipo -create openssl_i386/libssl.a openssl_x86_64/libssl.a -output libssl.a
 rm openssl-$OPENSSL_VERSION.tar.gz
