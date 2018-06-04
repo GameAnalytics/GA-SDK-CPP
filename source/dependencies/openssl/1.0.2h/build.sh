0a0da279cca0e0526849e20c54a1dd864200cbdb
Martin Treacy-Schwartz <mts@gameanalytics.com>
2018-06-04 14:01:15 +0200

    updated linux precompiled libs

--- source/dependencies/openssl/1.0.2h/build.sh
+++ source/dependencies/openssl/1.0.2h/build.sh
@@ -9,20 +9,22 @@ tar -xvzf openssl-$OPENSSL_VERSION.tar.gz
 mv openssl-$OPENSSL_VERSION openssl_x86_64
 cd openssl_i386
 # mac
-./Configure darwin-i386-cc -static
+#./Configure darwin-i386-cc -static
 
 # linux
-#./Configure -m32 linux-generic32 -static
+./config -m32 threads 386 no-asm no-shared no-rc4
+make depend
 make
 cd ../
 cd openssl_x86_64
 # mac
-./Configure darwin64-x86_64-cc -static
+#./Configure darwin64-x86_64-cc -static
 
 # linux
-#./Configure -m64 linux-generic64 -static
+./config -m64 threads no-asm no-shared no-rc4
+make depend
 make
 cd ../
-lipo -create openssl_i386/libcrypto.a openssl_x86_64/libcrypto.a -output libcrypto.a
-lipo -create openssl_i386/libssl.a openssl_x86_64/libssl.a -output libssl.a
+#lipo -create openssl_i386/libcrypto.a openssl_x86_64/libcrypto.a -output libcrypto.a
+#lipo -create openssl_i386/libssl.a openssl_x86_64/libssl.a -output libssl.a
 rm openssl-$OPENSSL_VERSION.tar.gz
