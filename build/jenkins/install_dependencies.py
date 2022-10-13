107b4afd674e34eb478973c13419b70631fa46e6
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-13 08:22:26 +0000

    Adding tarfile member sanitization to extractall()

--- build/jenkins/install_dependencies.py
+++ build/jenkins/install_dependencies.py
@@ -105,7 +105,26 @@ def untar(package, dst, silent=False):
     # with tarfile.TarFile(package, 'r') as zf:
     with tarfile.open(package, 'r') as zf:
         root_in_zip = zf.getnames()[0].split('/')[0]
-        zf.extractall()
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(zf)
         shutil.move(root_in_zip, dst)
     print('[done]')
 
