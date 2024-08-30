8f9f9dd8f223c948644ce1276a491609dbcd89ed
Leonid Pospelov <leonidpospelov.dev@gmail.com>
2024-08-30 21:51:08 +0500

    Update GADevice.cpp

--- source/gameanalytics/GADevice.cpp
+++ source/gameanalytics/GADevice.cpp
@@ -486,7 +486,7 @@ namespace gameanalytics
             #endif
 
                 snprintf(GADevice::_deviceModel, sizeof(GADevice::_deviceModel), "%s", modelName.c_str());
-                return true;
+                return;
             }
 
             std::cout << "Found device name:" << GADevice::_deviceModel;
